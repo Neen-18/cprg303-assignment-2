617b3b19cdf552c3cd778da338fa81d8be5fff97
Neen-18 <kuzonkaneki@gmail.com>
2026-02-03 11:18:25 -0700

    Removed extra folder

---
+++ app/Styles.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    profileCircle: {
+        height: 25,
+        width: 25,
+        borderRadius: '50%',
+        backgroundColor: red,
+    },
+
+    card: {
+        borderWidth: 2,
+        borderRadius: 14,
+        padding: 16,
+        gap: 2,
+  },
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { styles } from './Styles';
+
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Pressable onPress={() => alert("Alert message")}>
+          <Text>Alert!</Text>
+        </Pressable>
+
+      <View style = {styles.profileCircle}>test</View>
+
+      </View>
+    </SafeAreaView>
   );
 }
