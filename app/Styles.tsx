3793d4c9486d239340e2035ed8585b6fd2fb0610
Nathanael Lee <nathanael.lee@edu.sait.ca>
2026-02-05 13:19:10 -0700

    Made the containers

--- app/Styles.tsx
+++ app/Styles.tsx
@@ -1,23 +1,68 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    profileCircle: {
-        height: 25,
-        width: 25,
-        borderRadius: '70%',
-        backgroundColor: '#b22222',
-    },
+  Banner: {
+    height: "20%",
+    width: "100%",
+  },
 
-    card: {
-        borderWidth: 2,
-        borderRadius: 14,
-        padding: 16,
-        gap: 2,
-    },
+  profileCircle: {
+    height: 25,
+    width: 25,
+    borderRadius: "70%",
+    backgroundColor: "#b22222",
+  },
 
-    editProfile: {
-        height: 25,
-        width: 100,
-        backgroundColor: '#000000'
-    }
+  card: {
+    borderWidth: 2,
+    borderRadius: 14,
+    padding: 16,
+    gap: 2,
+  },
+
+  container: {
+    height: "80%",
+    width: "100%",
+    backgroundColor: "#222225",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  editProfile: {
+    height: "5%",
+    width: "90%",
+    borderRadius: 25,
+    backgroundColor: "#67666B",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 10,
+  },
+
+  orbsBar: {
+    height: "10%",
+    width: "90%",
+    borderRadius: 20,
+    backgroundColor: "#0A0A0A",
+    justifyContent: "center",
+    padding: 20,
+    margin: 10,
+  },
+
+  aboutMe: {
+    height: "20%",
+    width: "90%",
+    borderRadius: 20,
+    backgroundColor: "#0A0A0A",
+    padding: 20,
+    margin: 10,
+  },
+
+  connections: {
+    height: "20%",
+    width: "90%",
+    borderRadius: 20,
+    backgroundColor: "#0A0A0A",
+    padding: 20,
+    margin: 10,
+  },
 });
--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,84 @@
-import { Pressable, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Image, Pressable, Text, View } from "react-native";
 import { styles } from "./Styles";
-
 
 export default function Index() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={{
+    <View style={{ flex: 1 }}>
+      <View
+        style={{
           flex: 1,
           justifyContent: "center",
           alignItems: "center",
         }}
       >
+        {/* banner picture  */}
+        <View style={styles.Banner}>
+          <Image
+            source={{
+              uri: "https://cdn.discordapp.com/banners/356606310503940098/a_f16147d6886193e980601ec311b2c11b.png?size=480",
+            }}
+            style={{ height: "100%", width: "100%" }}
+          />
+        </View>
 
-      <View style = {styles.profileCircle}>
-        <Pressable onPress={() => alert("Alert Button pressed")}>
-          <Text>Alert</Text>
-        </Pressable>
-      </View>
+        {/* circle that holds the profile picture */}
+        <View style={styles.profileCircle}>
+          <Pressable onPress={() => alert("Alert Button pressed")}>
+            <Text>Alert</Text>
+          </Pressable>
+        </View>
 
+        {/* big box holding everything but the banner */}
+        <View style={styles.container}>
+          {/* edit profile box */}
+          <View style={styles.editProfile}>
+            <Text
+              style={{
+                color: "white",
+                fontSize: 18,
+              }}
+            >
+              Edit Profile
+            </Text>
+          </View>
+
+          {/* Orbs balance box */}
+          <View style={styles.orbsBar}>
+            <Text
+              style={{
+                color: "white",
+                fontSize: 18,
+              }}
+            >
+              Orbs Balance
+            </Text>
+          </View>
+
+          {/* About me box */}
+          <View style={styles.aboutMe}>
+            <Text
+              style={{
+                color: "white",
+                fontSize: 18,
+              }}
+            >
+              About Me
+            </Text>
+          </View>
+
+          {/* Connections box */}
+          <View style={styles.connections}>
+            <Text
+              style={{
+                color: "white",
+                fontSize: 18,
+              }}
+            >
+              Connections
+            </Text>
+          </View>
+        </View>
       </View>
-    </SafeAreaView>
+    </View>
   );
 }
